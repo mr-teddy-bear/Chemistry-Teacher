c812d4e0062ab11a8ef9e3ae874d16a9a1caee88
Maksim Sakalouski <Maksim.Sakalouski@itechart-group.com>
2021-02-23 17:14:05 +0300

    finished init the project

--- server/src/config/mongo.js
+++ server/src/config/mongo.js
@@ -1,18 +1,22 @@
-import mongoose from 'mongoose';
-import config from './environment.js';
+import mongoose from "mongoose";
+import config from "./environment.js";
 
-const { app: { port, mongoUri } } = config;
+const {
+  app: { port, mongoUri },
+} = config;
 
 export default async function initializeDbMongo(callback) {
-    try {
-        await mongoose.connect(mongoUri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        })
-        console.log(`MongoDB: Connection has been established successfully to port ${port}`)
-    } catch (e) {
-        console.log('SERVER ERROR', e.message)
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log(
+      `MongoDB: Connection has been established successfully to port ${port}`
+    );
+  } catch (e) {
+    console.log("SERVER ERROR", e.message);
+    process.exit(1);
+  }
 }
